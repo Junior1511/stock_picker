0de30522a287e9005ddddb60bf1538ea17bf02cc
Junior1511 <yunior122000@gmail.com>
2025-12-13 22:00:37 -0600

    omg this took like 3 hrs of thinking, I am exhausted lol, ill set the next project up and provably go to bed, good stuff, Im proud of myself. Keep it going kid

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,15 +1,22 @@
-def stock_picker (array)
-  lowest = []
-  profits = []
-  array.each_with_index do |number,day|
-    if day == 0 
-      lowest << [number,day]
-    elsif number < lowest.last.first
-      lowest << [number,day]
-    elsif number > lowest.last.first
-      profits << [(number - lowest.last.first), lowest.last.last, day]
+def stock_picker(array)
+  profit = []
+  buy = []
+  sell = []
+  array.each_with_index do |price, day|
+    if day == 0
+      buy = [price, day]
+    elsif day > 0 && price < buy[0]
+      buy = [price, day]
+    elsif day > buy[1] && price > buy[0]
+      sell = [price, day]
+      profit << [buy, sell, [(sell[0] - buy[0])]]
     end
   end
-  p [profits.max[1],profits.max[2]]
+  result = profit.max 
+  p result
+  puts "The best day to buy is day ##{result[0][1]} at a price of #{result[0][0]}"
+  puts "The best day to sell is day ##{result[1][1]} at a price of #{result[1][0]}"
+  puts "The profit of those days is #{result[2][0]}"
 end
- stock_picker([17,3,6,9,15,8,6,1,10])
+
+stock_picker([17,3,6,9,15,8,6,1,10])
